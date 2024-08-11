e12048da9e89d4701527b83f034e43261bd712d9
Yohanes Harke Wauran <harkewauran23@gmail.com>
2024-08-11 22:50:02 +0800

    refactor swagger

---
+++ src/configs/cors.config.ts
@@ -0,0 +1,7 @@
+// src/config/cors.config.ts
+
+export const CorsConfig = {
+  origin: '*', // Adjust this to specify allowed origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-// src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { CorsConfig } from './configs/cors.config'; // Make sure you have this config
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +15,23 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('api-docs', app, document, {
+    customSiteTitle: 'Task Management API',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+    ],
+  });
 
-  await app.listen(3000);
+  // CORS setup
+  const cors = { ...CorsConfig };
+  app.enableCors(cors);
+
+  await app.listen(5000);
 }
 bootstrap();
